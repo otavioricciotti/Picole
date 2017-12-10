4459482e3f19b00852c22deee28c531d6d603ffe
Otavio Ricciotti <otavioricciotti@gmail.com>
2017-12-10 01:45:05 -0200

    inicio form cadastro entidade

---
+++ Industria/Industria/frmEntidadeCadastro.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Industria
+{
+    partial class frmEntidadeCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFantasia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRazao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDOC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Location = new System.Drawing.Point(61, 12);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fantasia:";
+            // 
+            // txtFantasia
+            // 
+            this.txtFantasia.Location = new System.Drawing.Point(233, 12);
+            this.txtFantasia.Name = "txtFantasia";
+            this.txtFantasia.Size = new System.Drawing.Size(193, 20);
+            this.txtFantasia.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Razão social:";
+            // 
+            // txtRazao
+            // 
+            this.txtRazao.Location = new System.Drawing.Point(89, 38);
+            this.txtRazao.Name = "txtRazao";
+            this.txtRazao.Size = new System.Drawing.Size(337, 20);
+            this.txtRazao.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CPF/CNPJ:";
+            // 
+            // txtDOC
+            // 
+            this.txtDOC.Location = new System.Drawing.Point(80, 64);
+            this.txtDOC.Name = "txtDOC";
+            this.txtDOC.Size = new System.Drawing.Size(147, 20);
+            this.txtDOC.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tipo:";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(270, 63);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(156, 21);
+            this.cmbTipo.TabIndex = 9;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(15, 90);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(411, 23);
+            this.btnCadastrar.TabIndex = 10;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // frmEntidadeCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 124);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDOC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRazao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFantasia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEntidadeCadastro";
+            this.ShowIcon = false;
+            this.Text = "Cadastro de entidade";
+            this.Load += new System.EventHandler(this.frmEntidadeCadastro_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFantasia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRazao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDOC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Button btnCadastrar;
+    }
+}
